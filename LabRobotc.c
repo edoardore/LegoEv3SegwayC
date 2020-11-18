9e3f91828bc703c20b03eaa9e6d7c4d2658bedd8
Edoardo Re <edoardore@me.com>
2020-11-18 18:43:37 +0100

    Add files via upload
    Corretti errori, robot più stabile, Ts raggiunto 0.0008 secondi

--- LabRobotc.c
+++ LabRobotc.c
@@ -4,10 +4,10 @@ float control(float Theta, float Psi, float Theta_dot, float Psi_dot, float Thet
 
 task main()
 {
-	float offset=calibrate_gyro(5)*3.14/180;
+	float offset=calibrate_gyro(5)*PI/180;
 	wait1Msec(1000);
-	int ThetaTimeList[2];
-	int Psi_dotTimeList[2];
+	float ThetaTimeList[2];
+	float Psi_dotTimeList[2];
 	float u;
 	float ThetaList[2];
 	ThetaList[0]=0;
@@ -26,16 +26,18 @@ task main()
 	float angleDx;
 	float angleSx;
 	float ThetaM;
-	int t;
-	int millisecondsStart=time1[T1];
+	float t;
+	float millisecondsStart=time1[T1];
 	while(true){
 		t=time1[T1];
-		if((t-millisecondsStart)>10){
+		if((t-millisecondsStart)>10000){
+			moveMotorTarget(motorB, 110, -100);
+			count++;
 			break;
 		}
 		if(count==0){
-			Theta=0;
-			Psi=-8.7*3.14/180;
+			Theta=8.7*PI/180;
+			Psi=8.7*PI/180;
 			Theta_dot=0;
 			Psi_dot=0;
 			Theta_int=0;
@@ -51,36 +53,40 @@ task main()
 			u=-100;
 		setMotorSpeed(motorA, u);
 		setMotorSpeed(motorD, u);
-		if (count==1){
+		if (count==100){
 			//rackUp
-			moveMotorTarget(motorB, 90, 500);
+			moveMotorTarget(motorB, 110, 100);
+
 		}
-		//sensor
 		ThetaTimeList[0]=ThetaTimeList[1];
 		Psi_dotTimeList[0]=Psi_dotTimeList[1];
+		//sensor
+		int i=0;
 		angleDx=getMotorEncoder(motorA);
 		angleSx=getMotorEncoder(motorD);
+
 		ThetaTimeList[1]=time1[T1];
 		ThetaM=(angleDx+angleSx)/2;
-		ThetaM=ThetaM*3.14/180;
+		ThetaM=ThetaM*PI/180;
 		Psi_dot=getGyroRate(S4);
 		Psi_dotTimeList[1]=time1[T1];
-		Psi_dot=Psi_dot*3.14/180;
+		Psi_dot=Psi_dot*PI/180;
 		Psi_dot=Psi_dot-offset;
 		Psi_dotList[0]=Psi_dotList[1];
 		Psi_dotList[1]=Psi_dot;
-		integrate_Psi+=Psi_dotList[1]*(Psi_dotTimeList[1]-Psi_dotTimeList[0]);
+		//integrate_Psi+=Psi_dotList[1]*(Psi_dotTimeList[1]-Psi_dotTimeList[0])/1000;
+		integrate_Psi+=Psi_dotList[1]*0.0009;
 		Psi=integrate_Psi;
 		Theta=ThetaM+Psi;
 		ThetaList[0]=ThetaList[1];
 		ThetaList[1]=Theta;
-		Theta_dot=(ThetaList[1]-ThetaList[0])/(ThetaTimeList[1]-ThetaTimeList[0]);
-		integrate_Theta+=ThetaList[1]*(ThetaTimeList[1]-ThetaTimeList[0]);
+		//Theta_dot=(ThetaList[1]-ThetaList[0])*1000/(ThetaTimeList[1]-ThetaTimeList[0]);
+		Theta_dot=(ThetaList[1]-ThetaList[0])/0.0009;
+		//integrate_Theta+=ThetaList[1]*(ThetaTimeList[1]-ThetaTimeList[0])/1000;
+		integrate_Theta+=ThetaList[1]*0.0009;
 		Theta_int=integrate_Theta;
+
 	}
-	//rackDown
-	moveMotorTarget(motorB, -90, 500);
-	setMotorTarget(motorB, -90, 500);
 }
 
 
@@ -102,7 +108,6 @@ float calibrate_gyro(int loops){
 	mean=mean/loops;
 	return mean;
 }
-
 
 float control(float Theta, float Psi, float Theta_dot, float Psi_dot,float Theta_int){
 	float u=(-0.8559*Theta)+(-44.7896*Psi)+(-0.9936*Theta_dot)+(-4.6061*Psi_dot)+(-0.500*Theta_int);
